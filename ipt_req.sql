3e9aa8829d3ca47ab8d372ba2e1dcd57916bc856
nongdi177013 <149787913+nongdi177013@users.noreply.github.com>
2024-05-12 14:06:03 +0800

    Add files via upload

---
+++ ipt_req.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2024 at 07:19 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ipt_req`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_data`
+--
+
+CREATE TABLE `student_data` (
+  `id` int(10) NOT NULL,
+  `u_card` varchar(12) NOT NULL,
+  `u_f_name` text NOT NULL,
+  `u_l_name` text NOT NULL,
+  `u_father` text NOT NULL,
+  `u_aadhar` varchar(12) NOT NULL,
+  `u_birthday` text NOT NULL,
+  `u_gender` varchar(6) NOT NULL,
+  `u_email` text NOT NULL,
+  `u_phone` varchar(10) NOT NULL,
+  `u_state` varchar(12) NOT NULL,
+  `u_dist` text NOT NULL,
+  `u_village` text NOT NULL,
+  `u_police` text NOT NULL,
+  `u_pincode` text NOT NULL,
+  `file` longblob NOT NULL,
+  `u_mother` varchar(30) NOT NULL,
+  `u_family` text NOT NULL,
+  `staff_id` varchar(12) NOT NULL,
+  `image` varchar(150) NOT NULL,
+  `uploaded` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_data`
+--
+
+INSERT INTO `student_data` (`id`, `u_card`, `u_f_name`, `u_l_name`, `u_father`, `u_aadhar`, `u_birthday`, `u_gender`, `u_email`, `u_phone`, `u_state`, `u_dist`, `u_village`, `u_police`, `u_pincode`, `file`, `u_mother`, `u_family`, `staff_id`, `image`, `uploaded`) VALUES
+(124, '2022100160', 'Ernest', 'Daus', 'Father', '20', '2024-05-30', 'Male', 'daus@gmail.com', '0968241335', '', 'Philippines', 'Camiling', 'Camiling', '2306', '', 'Mother', 'C.', 'BSIT', '4.png', '2024-05-12 00:26:02');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_data`
+--
+ALTER TABLE `student_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_data`
+--
+ALTER TABLE `student_data`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
